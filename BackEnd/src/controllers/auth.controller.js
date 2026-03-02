fb3fb852eb5030a275bd8c1a2988c43217d1a89a
AshaduzzamanAbira <110991008+AshaduzzamanAbir@users.noreply.github.com>
2026-03-03 00:33:06 +0600

    change folder

---
+++ BackEnd/src/controllers/auth.controller.js
@@ -0,0 +1,150 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const blacklistTokenModel = require("../models/blacklist.model");
+
+/**
+ *
+ * @name registerUserControler
+ * @description register a new user,excpects username, email , password is require
+ * @access Public
+ */
+
+async function registerUserController(req, res) {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      message: "Please provide all required fields",
+    });
+  }
+
+  const isUserAlreadyExist = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserAlreadyExist) {
+    return res.status(400).json({
+      message: "User already exist with this email and username",
+    });
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await userModel.create({
+      username,
+      email,
+      password: hash,
+    });
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.cookie("token", token);
+
+    return res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+}
+
+/**
+ *
+ * @name loginUserController
+ * @description login user excpects email and password
+ * @access Public
+ *
+ */
+async function loginUserController(req, res) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(400).json({
+      message: "User and password does not match",
+    });
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatch) {
+    return res.status(400).json({
+      message: "User and password does not match",
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  return res.status(200).json({
+    message: "User logged in successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+}
+
+/**
+ *
+ * @name logoutUserController
+ * @description logout user
+ * @access Public
+ *
+ */
+
+async function logoutUserController(req, res) {
+  const token = req.cookies.token;
+
+  if (token) {
+    await blacklistTokenModel.create({ token });
+    res.clearCookie("token");
+    return res.status(200).json({
+      message: "User logged out successfully",
+    });
+  }
+}
+
+/**
+ * @name getMeController
+ * @description get the current logged in user details.
+ * @access privet
+ */
+
+async function getMeController(req, res) {
+  const user = await userModel.findById(req.user.id);
+
+  res.status(200).json({
+    message: "User details fetch successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+}
+
+module.exports = {
+  registerUserController,
+  loginUserController,
+  logoutUserController,
+  getMeController,
+};
